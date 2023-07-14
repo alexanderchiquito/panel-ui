d3f507a2ee56ef7fdfffcdb4f7a02a68dbeeb83b
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-07-14 17:09:12 -0500

    fix pages components

---
+++ react-admin/src/App.tsx
@@ -0,0 +1,30 @@
+import Home from "./pages/home/Home"
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Home/>
+    },
+    {
+      path: "users",
+      element: <Users/>,
+    },
+    {
+      path: "products",
+      element: <Products/>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />
+}
+
+export default App
