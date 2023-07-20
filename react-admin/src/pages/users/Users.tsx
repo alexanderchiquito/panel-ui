9608487626ada5fe0ffc7be71ab860a99aa9e516
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-07-20 14:51:57 -0500

    fix users, products

--- react-admin/src/pages/users/Users.tsx
+++ react-admin/src/pages/users/Users.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import DataTable from "../../components/dataTable/DataTable";
 import { userRows } from "../../data";
 import "./users.scss";
 import { GridColDef } from "@mui/x-data-grid";
+import Add from "../../components/add/Add";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "avatar",
+    field: "img",
     headerName: "Avatar",
     width: 100,
     renderCell: (params) => {
@@ -70,13 +72,16 @@ const columns: GridColDef[] = [
 ];
 
 const Users = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={()=> setOpen(true)} >Add New User</button>
       </div>
       <DataTable slug='users' columns={columns} rows={userRows} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
 };
