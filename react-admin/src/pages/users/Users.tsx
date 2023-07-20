449c87b25489f8a867d908b88ea9aebfa603aba6
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-07-20 10:11:39 -0500

    fix users component

--- react-admin/src/pages/users/Users.tsx
+++ react-admin/src/pages/users/Users.tsx
@@ -1,8 +1,84 @@
-import './users.scss'
+import DataTable from "../../components/dataTable/DataTable";
+import { userRows } from "../../data";
+import "./users.scss";
+import { GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "./noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    type: "string",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+  // {
+  //   field: "actions",
+  //   headerName: "Actions",
+  //   width: 100,
+  //   renderCell: (params) =>{
+  //       return <div className="action">
+  //           <div className="view">View</div>
+  //           <div className="delete">Delete</div>
+  //       </div>
+  //   }
+  // },
+  // {
+  //   field: "fullName",
+  //   headerName: "Full name",
+  //   description: "This column has a value getter and is not sortable.",
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params: GridValueGetterParams) =>
+  //   `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  // },
+];
+
 const Users = () => {
   return (
-    <div>Users</div>
-  )
-}
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug='users' columns={columns} rows={userRows} />
+    </div>
+  );
+};
 
-export default Users
+export default Users;
